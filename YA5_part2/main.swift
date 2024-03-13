c82d72199d4eca7c304281075f31502999998837
IrinaMuravyova <miomir@yandex.ru>
2024-03-13 17:59:03 +0800

    taskA is completed

--- YA5_part2/main.swift
+++ YA5_part2/main.swift
@@ -5,7 +5,37 @@
 //  Created by Irina Muravyeva on 13.03.2024.
 //
 
-import Foundation
+//import Foundation
 
-print("Hello, World!")
+let k = Int(readLine() ?? "") ?? 0
+var pointsX = [Int]()
+var pointsY = [Int]()
 
+for _ in 1...k {
+    let point = readLine() ?? ""
+    pointsX.append(Int(point.split(separator: " ")[0]) ?? 0)
+    pointsY.append(Int(point.split(separator: " ")[1])  ?? 0)
+}
+
+var minX = pointsX[0]
+var maxX = pointsX[0]
+var minY = pointsY[0]
+var maxY = pointsY[0]
+
+for x in pointsX {
+    if x < minX {
+        minX = x
+    } else if x > maxX {
+        maxX = x
+    }
+}
+
+for y in pointsY {
+    if y < minY {
+        minY = y
+    } else if y > maxY {
+        maxY = y
+    }
+}
+
+print(minX, minY, maxX, maxY)
