0d952ac507f0cb1cb993cb7377785f5df516e4e3
IrinaMuravyova <120377570+IrinaMuravyova@users.noreply.github.com>
2024-04-13 16:51:19 +0800

    Update task2A.swift, all code included in func

--- Lesson_2/task2A.swift
+++ Lesson_2/task2A.swift
@@ -7,36 +7,37 @@
 
 //TaskA. Минимальный прямоугольник
 
-
-let k = Int(readLine() ?? "") ?? 0
-var pointsX = [Int]()
-var pointsY = [Int]()
-
-for _ in 1...k {
-    let point = readLine() ?? ""
-    pointsX.append(Int(point.split(separator: " ")[0]) ?? 0)
-    pointsY.append(Int(point.split(separator: " ")[1])  ?? 0)
-}
-
-var minX = pointsX[0]
-var maxX = pointsX[0]
-var minY = pointsY[0]
-var maxY = pointsY[0]
-
-for x in pointsX {
-    if x < minX {
-        minX = x
-    } else if x > maxX {
-        maxX = x
+func coordinates() {
+    let k = Int(readLine() ?? "") ?? 0
+    var pointsX = [Int]()
+    var pointsY = [Int]()
+    
+    for _ in 1...k {
+        let point = readLine() ?? ""
+        pointsX.append(Int(point.split(separator: " ")[0]) ?? 0)
+        pointsY.append(Int(point.split(separator: " ")[1])  ?? 0)
     }
-}
-
-for y in pointsY {
-    if y < minY {
-        minY = y
-    } else if y > maxY {
-        maxY = y
+    
+    var minX = pointsX[0]
+    var maxX = pointsX[0]
+    var minY = pointsY[0]
+    var maxY = pointsY[0]
+    
+    for x in pointsX {
+        if x < minX {
+            minX = x
+        } else if x > maxX {
+            maxX = x
+        }
     }
+    
+    for y in pointsY {
+        if y < minY {
+            minY = y
+        } else if y > maxY {
+            maxY = y
+        }
+    }
+    
+    print(minX, minY, maxX, maxY)
 }
-
-print(minX, minY, maxX, maxY)
